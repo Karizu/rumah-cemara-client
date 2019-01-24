5b13bdb1cb86508d07bbea56575f3e833a5595ed
nurwin <nurwin.jtk10@gmail.com>
2019-01-24 16:50:04 +0700

    add new gradle plugin for API

---
+++ app/src/main/java/cemara/labschool/id/rumahcemara/api/ApiInterface.java
@@ -0,0 +1,17 @@
+package cemara.labschool.id.rumahcemara.api;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    String BASE_URL = "http://37.72.172.144/superfriends-api/public/api/";
+
+
+}
