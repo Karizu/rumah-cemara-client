0b29e38715f1dbd4ef3dbc76a8d81f5b10fd1b65
Lu'lu' Al-Maknun <almaknunlulu6@gmail.com>
2019-01-25 12:25:05 +0700

    Add Ask The Expert Activity Layout

--- app/src/main/java/cemara/labschool/id/rumahcemara/home/service/asktheexpert/AskTheExpertActivity.java
+++ app/src/main/java/cemara/labschool/id/rumahcemara/home/service/asktheexpert/AskTheExpertActivity.java
@@ -2,14 +2,43 @@ package cemara.labschool.id.rumahcemara.home.service.asktheexpert;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cemara.labschool.id.rumahcemara.R;
+import cemara.labschool.id.rumahcemara.util.nearest.adapter.NearestAdapter;
+import cemara.labschool.id.rumahcemara.util.nearest.modal.Nearest;
 
 public class AskTheExpertActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ask_the_expert_activity);
+        ButterKnife.bind(this);
+        setToolbar();
+    }
+
+    public void setToolbar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(R.drawable.icon_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
