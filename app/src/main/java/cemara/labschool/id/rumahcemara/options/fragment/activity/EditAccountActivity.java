f79c4b22d25bb074f147911c630fdfb3f07ffcd4
Lu'lu' Al-Maknun <almaknunlulu6@gmail.com>
2019-01-25 07:45:18 +0700

    Add Edit Acc Activity

---
+++ app/src/main/java/cemara/labschool/id/rumahcemara/options/fragment/activity/EditAccountActivity.java
@@ -0,0 +1,51 @@
+package cemara.labschool.id.rumahcemara.options.fragment.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cemara.labschool.id.rumahcemara.R;
+
+public class EditAccountActivity extends AppCompatActivity {
+
+    @BindView (R.id.toolbar)
+    Toolbar toolbar;
+    @BindView (R.id.text_toolbar_title)
+    TextView toolbarTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_account);
+        ButterKnife.bind(this);
+        setToolbar();
+    }
+
+    public void setToolbar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(R.drawable.icon_back);
+        toolbarTitle.setText("Edit Account");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //What to do on back clicked
+                onBackPressed();
+            }
+        });
+    }
+
+    @OnClick(R.id.save)
+    public void toOption(){
+        finish();
+    }
+}
