de7a3e9a70799e91e647d1b06994c039f6e8a0cc
Lu'lu' Al-Maknun <almaknunlulu6@gmail.com>
2019-01-25 18:09:09 +0700

    Ask The Expert

--- app/src/main/java/cemara/labschool/id/rumahcemara/home/service/asktheexpert/ChatExpertActivity.java
+++ app/src/main/java/cemara/labschool/id/rumahcemara/home/service/asktheexpert/ChatExpertActivity.java
@@ -1,32 +1,17 @@
 package cemara.labschool.id.rumahcemara.home.service.asktheexpert;
 
-import android.app.Dialog;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 import cemara.labschool.id.rumahcemara.R;
-import cemara.labschool.id.rumahcemara.util.nearest.adapter.NearestAdapter;
-import cemara.labschool.id.rumahcemara.util.nearest.modal.Nearest;
 
-public class AskTheExpertActivity extends AppCompatActivity {
+public class ChatExpertActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -34,7 +19,7 @@ public class AskTheExpertActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ask_the_expert_activity);
+        setContentView(R.layout.activity_chat_expert);
         ButterKnife.bind(this);
         setToolbar();
     }
@@ -50,10 +35,4 @@ public class AskTheExpertActivity extends AppCompatActivity {
             }
         });
     }
-
-    @OnClick(R.id.topiclist)
-    public void createTopic(View view) {
-        startActivity(new Intent(getApplicationContext(), ChatExpertActivity.class));
-    }
-
 }
